6ccfda2af29a499acc668c1efedf9682ece9f962
Robin <rjlb991102@hotmail.com>
2024-11-03 00:05:17 -0500

    first commit.

---
+++ Desafio/Program.cs
@@ -0,0 +1,11 @@
+﻿// Desafío de código: Notificar los identificadores de pedido que necesitan
+// una investigación más detallada
+
+string[] orderIDs = { "B123", "C234", "A345", "B177", "G3003", "C235", "B179" };
+foreach (var orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
---
+++ foreach/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] nombres = {"Maria", "Joel", "Elizabeth", "Eugenia"};
+foreach (var nombre in nombres)
+{
+    Console.WriteLine(nombre);
+}
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+
+foreach (var item in inventory)
+{
+    sum += item;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {item} items (Running total: {sum})");
+}
+
+Console.WriteLine($"La suma total de 'inventory' es {sum}");
